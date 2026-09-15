d2d71dff7c6e5df971de24f3069b9ebe2f14ec3c
otiski <otis.veraghtert@gmail.com>
2026-09-15 20:07:20 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function verdeelTaken() {
+    let taken = [
+        'Balken leggen',
+        'binnen roepen',
+        'omroepen',
+        'mest scheppen',
+        'jury',
+        'jury dressuur',
+    ]
+    let resultaatLijst = document.getElementById('resultaat')
+
+    resultaatLijst.innerHTML = ''
+
+    let aantal = parseInt(prompt('Hoeveel leden?'))
+
+    for (let i = 0; i < aantal; i++) {
+        let naam = prompt('Naam lid')
+
+        let menu = 'Kies taak uit menu: \n'
+        taken.forEach((taak, index) => {
+            menu += `${index + 1}: ${taak}\n`
+        })
+
+        let gekozenNummer = parseInt(prompt(menu)) - 1
+
+        let gekozenTaak = taken.splice(gekozenNummer, 1)[0]
+
+        resultaatLijst.innerHTML += `<li><strong>${naam}</strong>: ${gekozenTaak}</li>`
+    }
+}
